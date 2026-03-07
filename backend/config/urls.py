41cd703f85c8f9142be3bed29c3fd1a26c610f75
suraj <sk3057448@gmail.com>
2026-03-07 16:41:44 +0530

    deployment setup

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +11,6 @@ urlpatterns = [
 
     # ✅ NEW
     path("api/subjects/", include("subjects.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
+
 ]
